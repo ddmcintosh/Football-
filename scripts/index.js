16fc318c95c8891024fb23289222c0b710af856b
Your Name <you@example.com>
2022-11-03 09:47:16 -0500

    completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,87 @@
-"use strict"
+"use strict";
+
+window.onload = init;
 
 
-    let teams = [
-        {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
-        {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
-        {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-        {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
-    ];
+const teams = [
+  { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+  { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+  { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+  { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
+];
 
-       let codes = [];
-       let names = [];
-       let plays = [];
+function init() {
+  console.log("index.js");
+  fillDropdown();
+  const selectBtn = document.getElementById("selectBtn");
+  selectBtn.onclick = selectBtnOnClick;
+  //document.getElementById("selectBtn") = selectBtnOnClick;
 
-        for (let value of teams) {
-         codes.push(value.code);
-         names.push(value.name);
-         plays.push(value.plays);
-        }
+}
 
-        const teamsList = document.getElementById("footballTeam");
+function fillDropdown() { // To create the dropdown menu
+  const teamSelect = document.getElementById("teamSelect");
+  let selectTeamOption = document.createElement("option");
+  selectTeamOption.value = "";
+  selectTeamOption.textContent = "Select a Team...";
+  teamSelect.appendChild(selectTeamOption);
 
-        let length = teams.length 
+  let teamsLength = teams.length;
+  for (let i = 0; i < teamsLength; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = teams[i].code;
+    newOption.textContent = teams[i].name;
 
-        window.onload = function() {
-            initTeamsDropdown() 
-            const SubmitBtn = document.getElementById("submitBtn");
-            SubmitBtn.onclick = displayInfo;
+    teamSelect.appendChild(newOption);
+  }
 
+}
 
-        }
-        function initTeamsDropdown() {
+function selectBtnOnClick() {
+  const teamSelect = document.getElementById("teamSelect");
+  let selectedTeamCode = teamSelect.value;
+  let selectedTeam = getTeamFromCode(teams, selectedTeamCode);
 
-            for (let i = 0; i < length; i++) {
-                // create the option element 
-                let theOption = document.createElement("option")
-    
-                //set the text & value 
-                theOption.textContent = names[i];
-                theOption.value = codes[i];
-    
-                // append option 
-                footballTeam.appendChild(theOption);
-            }
+  let message = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}.`
+  const teamInfo = document.getElementById("teamInfo");
+  teamInfo.innerHTML = message;
+  return false;
+}
 
-        }
-    function displayInfo() {
-        let selectedValue = footballTeam.value;
-        for (let i = 0; i < length; i++) {
-            if (selectedValue == codes[i]) {
-                document.getElementById("teamInfo").innerHTML = " You selected the " + names[i] + codes[i] + " who play in " + locations[i];
-            }
-        }
-        return false;
+function getTeamFromCode(teams, code) { 
+  let teamscount = teams.length;
+  for (let i = 0; i < teamscount; i++) {
+    if (teams[i].code == code) {
+      return teams[i];
     }
+  }
+  return null;
+}
+
+// function selectBtnOnClick()
+// {
+//   const teamSelect = document.getElementById("teamSelect");
+//   teamSelect.code = "DAL";
+
+//   let length = teamSelect.options.length;
+//   for (let i = 0;i < length; i++)
+//   {
+//     if (teamSelect.options[i].name == .code)
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
